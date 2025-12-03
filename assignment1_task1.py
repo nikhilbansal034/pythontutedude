8e8d6f3328452f6c0448f19410d90d362c3e8299
nikhilbansal034 <nikhilbansal034@gmail.com>
2025-12-03 17:41:32 +0530

    initial commit

---
+++ assignment1_task1.py
@@ -0,0 +1,13 @@
+# input variables
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+#operational variables
+addition = num1 + num2
+subtraction = num1 - num2
+Multiplication = num1 * num2
+Division = num1/ num2
+#output
+print('Addition: ', addition)
+print('Subtraction: ', subtraction)
+print('Multiplication: ', Multiplication)
+print('Division: ',Division)
