47ed824217f283478babb5fc446950adf6e437ef
nikhilbansal034 <nikhilbansal034@gmail.com>
2026-09-17 12:54:01 +0530

    Delete assignment1_task2.py

--- assignment1_task2.py
+++
@@ -1,5 +0,0 @@
-#input variable
-first_name = input('Enter your first name: ',)
-last_name = input('Enter your last name: ')
-#output
-print('Hello, ', first_name + ' ' + last_name + '!', 'Welcome to the python program.')
